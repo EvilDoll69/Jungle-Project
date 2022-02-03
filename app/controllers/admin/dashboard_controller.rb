6b73653b63dbad4807613d235b2c19befeb4bb79
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-02-03 01:31:36 +0000

    Added tables to dasgboard which show number of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   
-
   def show
+    @count_products = Product.count(:quantity)
+    @count_category = Category.count(:id)    
   end
+  
 end
